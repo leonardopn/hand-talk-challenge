07d79448340cdd17dcab36bb481dee4d7fe54d1e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-07-28 10:51:20 -0300

    chore: :lipstick: Add all color palette

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -7,11 +7,79 @@ const config: Config = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+		colors: {
+			brand: {
+				primary: {
+					main: "#F5F5F5",
+					light: "#c4c4c4",
+					medium: "#666666",
+					dark: "#333333",
+				},
+				secondary: {
+					main: "#F06F06",
+					lightest: "#FBC5A0",
+					light: "#FAAE4B",
+					medium: "#FB8C01",
+					dark: "#CC5E05",
+				},
+			},
+			highlight: {
+				primary: {
+					main: "#413987",
+					lightest: "#E0E6F9",
+					light: "#5B69C2",
+					medium: "#542DC2",
+					dark: "#121A5C",
+				},
+			},
+			neutral: {
+				low: {
+					main: "#333333",
+					lightest: "#979797",
+					light: "#818181",
+					medium: "#666666",
+					dark: "#000000",
+				},
+				high: {
+					main: "#F5F5F5",
+					lightest: "#ffffff",
+					light: "#DEDEDE",
+					medium: "#C4C4C4",
+					dark: "#AEAEAE",
+				},
+			},
+			feedback: {
+				success: {
+					main: "#20C6AD",
+					light: "#CCFCDE",
+					medium: "#00E9B4",
+					dark: "#006666",
+				},
+				info: {
+					main: "#3B98D2",
+					light: "#DCFCFE",
+					medium: "#48C1F8",
+					dark: "#103C75",
+				},
+				alert: {
+					main: "#FCC002",
+					lightest: "#FCF6B2",
+					light: "#FCFA37",
+					medium: "#FBD24F",
+					dark: "#CA9A00",
+				},
+				error: {
+					main: "#FF4D4D",
+					lightest: "#FFB3B3",
+					light: "#FF8080",
+					medium: "#FF6666",
+					dark: "#B23535",
+				},
+			},
+			accessibility: {
+				primary: {
+					main: "#003087",
+				},
 			},
 		},
 	},
