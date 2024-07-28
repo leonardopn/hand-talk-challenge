244827188da53eae0b1290675df047a324a709f8
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-07-28 17:11:57 -0300

    feat(Web: Button): :sparkles: Allow component has variants

--- packages/frontend/src/components/Button/index.tsx
+++ packages/frontend/src/components/Button/index.tsx
@@ -1,19 +1,27 @@
 import { twMerge } from "tailwind-merge";
+import { tv } from "tailwind-variants";
+
+const buttonVariant = tv({
+	base: "py-nano px-xxxs font-semiBold rounded-xs transition-colors focus:outline-none focus:ring-2 text-brand-primary-main",
+	variants: {
+		color: {
+			primary:
+				"bg-brand-secondary-main hover:bg-brand-secondary-medium active:bg-brand-secondary-light focus:ring-highlight-primary-light",
+			purple: "bg-highlight-primary-main hover:bg-highlight-primary-medium active:bg-highlight-primary-light focus:ring-brand-secondary-light",
+		},
+	},
+});
 
 interface ButtonProps extends React.ComponentProps<"button"> {
 	"aria-label": string;
+	colorScheme?: keyof (typeof buttonVariant)["variants"]["color"];
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, colorScheme = "primary", ...props }: ButtonProps) {
 	return (
 		<button
 			{...props}
-			className={twMerge(
-				`bg-brand-secondary-main text-brand-primary-main py-nano px-xxxs font-semiBold rounded-xs
-				hover:bg-brand-secondary-medium active:bg-brand-secondary-light transition-colors focus:outline-none
-				focus:ring-2 focus:ring-highlight-primary-light`,
-				props.className
-			)}>
+			className={twMerge(buttonVariant({ color: colorScheme }), props.className)}>
 			{children}
 		</button>
 	);
