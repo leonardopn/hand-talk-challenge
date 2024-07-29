7abf54ca6240a03c500f0896751b9bf870eb66a3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-07-28 21:49:17 -0300

    feat(Web: ThemeModeToggle): :sparkles: Do the switcher get user's system preference of theme color

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="pt-BR">
+		<html lang="pt-BR" suppressHydrationWarning>
 			<body
 				className={twMerge(
 					lato.className,
--- packages/frontend/src/components/ThemeModeToggle/index.tsx
+++ packages/frontend/src/components/ThemeModeToggle/index.tsx
@@ -1,28 +1,47 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { LoaderCircle, Moon, Sun, SunMoon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { tv } from "tailwind-variants";
 
 const buttonVariant = tv({
 	base: "animate-in fade-in size-6 transition-colors cursor-pointer text-neutral-high-lightest",
-	variants: {
-		color: {
-			dark: "hidden dark:inline",
-			light: "dark:hidden",
-		},
-	},
 });
 
 export function ThemeModeToggle() {
-	const { setTheme } = useTheme();
+	const { setTheme, themes, theme } = useTheme();
+
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	const setNextTheme = useCallback(() => {
+		const currentThemeIndex = themes.indexOf(theme || "system");
+		const nextThemeIndex = (currentThemeIndex + 1) % themes.length;
+		const nextTheme = themes[nextThemeIndex];
+
+		setTheme(nextTheme);
+	}, [setTheme, theme, themes]);
+
+	const currentIconTheme = useMemo(() => {
+		switch (theme) {
+			case "light":
+				return <Sun className={buttonVariant()} onClick={setNextTheme} />;
+			case "dark":
+				return <Moon className={buttonVariant()} onClick={setNextTheme} />;
+			default:
+				return <SunMoon className={buttonVariant()} onClick={setNextTheme} />;
+		}
+	}, [setNextTheme, theme]);
 
 	return (
 		<button
 			className="focus:ring-highlight-primary-light hover:scale-110 transition-transform"
 			aria-label="Botão para alternar entre tema claro e escuro">
-			<Sun className={buttonVariant({ color: "light" })} onClick={() => setTheme("dark")} />
-			<Moon className={buttonVariant({ color: "dark" })} onClick={() => setTheme("light")} />
+			{isClient ? currentIconTheme : <LoaderCircle className="animate-spin" />}
 		</button>
 	);
 }
