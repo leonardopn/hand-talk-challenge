b96d348a768a2c5d2132830b5c767ceace82fb4d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-07-28 22:15:40 -0300

    fix(Web: GoToTopButton): :rotating_light: Fix hydratation error

--- packages/frontend/src/components/GoToTopButton/index.tsx
+++ packages/frontend/src/components/GoToTopButton/index.tsx
@@ -3,25 +3,31 @@
 import { ArrowBigUp } from "lucide-react";
 import { Button } from "../Button";
 import { useWindowScroll } from "react-use";
+import { useState, useEffect } from "react";
 
 export function GoToTopButton() {
+	const [isClient, setIsClient] = useState(false);
 	const { y } = useWindowScroll();
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
 	function goToTop() {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	}
 
-	if (y <= 20) {
-		return null;
+	if (y >= 20 && isClient) {
+		return (
+			<Button
+				onClick={goToTop}
+				aria-label="Botão voltar para o topo"
+				colorScheme="purple"
+				className="fixed bottom-3 right-3 px-2 py-2 animate-in fade-in">
+				<ArrowBigUp />
+			</Button>
+		);
 	}
 
-	return (
-		<Button
-			onClick={goToTop}
-			aria-label="Botão voltar para o topo"
-			colorScheme="purple"
-			className="fixed bottom-3 right-3 px-2 py-2 animate-in fade-in">
-			<ArrowBigUp />
-		</Button>
-	);
+	return null;
 }
