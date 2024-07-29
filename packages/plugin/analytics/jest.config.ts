fbe0314762e05bfabeea49d6a24e08741bcaee01
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-07-29 20:12:08 -0300

    chore: :wrench: Update jest confg to includes parsers for virtual dom

--- packages/plugin/analytics/jest.config.ts
+++ packages/plugin/analytics/jest.config.ts
@@ -3,10 +3,17 @@ import type { Config } from "jest";
 const config: Config = {
 	preset: "ts-jest",
 	coverageProvider: "v8",
-	testEnvironment: "node",
+	testEnvironment: "jsdom",
 	roots: ["<rootDir>/src"],
 	testMatch: ["**/?(*.)+(spec|test).[t]s?(x)"],
-	moduleFileExtensions: ["ts", "js"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+	transform: {
+		"^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+	},
+	moduleNameMapper: {
+		"\\.(css|less|scss|sass)$": "identity-obj-proxy",
+	},
 };
 
 export default config;
